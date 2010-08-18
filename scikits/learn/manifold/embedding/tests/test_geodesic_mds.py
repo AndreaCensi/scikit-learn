290b136c1a19fe81751e63ed5df6ee3e216c5567
Matthieu Brucher <matthieu.brucher@gmail.com>
2010-08-18 21:34:00 +0200

    Added tests for Geodesic NLM

--- scikits/learn/manifold/embedding/tests/test_geodesic_mds.py
+++ scikits/learn/manifold/embedding/tests/test_geodesic_mds.py
@@ -13,7 +13,7 @@ from nose.tools import raises
 
 from ..tools import create_neighborer, dist2hd
 from ..geodesic_mds import reduct, populate_distance_matrix_from_neighbors, \
-    Isomap
+    Isomap, GeodesicNLM
 
 samples = numpy.array((0., 0., 0.,
   1., 0., 0.,
@@ -68,7 +68,7 @@ class TestIsomap(TestCase):
         assert(isomap.fit(samples[:3]) == isomap)
         assert(hasattr(isomap, 'embedding_'))
         assert(isomap.embedding_.shape == (3, 2))
-        assert_array_almost_equal(dist2hd(isomap.embedding_[:3], 
+        assert_array_almost_equal(dist2hd(isomap.embedding_[:3],
             isomap.embedding_[:3])**2, distances[:3, :3])
 
     @raises(RuntimeError)
@@ -82,6 +82,27 @@ class TestIsomap(TestCase):
         isomap.fit(samples[:3])
         mapped = isomap.transform(samples)
         assert_array_almost_equal(mapped[:3], isomap.embedding_, decimal=3)
+
+class TestGeodesicNLM(TestCase):
+    def test_fit(self):
+        nlm = GeodesicNLM(n_coords = 2, mapping_kind = None, n_neighbors = 3)
+        assert(nlm.fit(samples[:3]) == nlm)
+        assert(hasattr(nlm, 'embedding_'))
+        assert(nlm.embedding_.shape == (3, 2))
+        assert_array_almost_equal(dist2hd(nlm.embedding_[:3],
+            nlm.embedding_[:3])**2, distances[:3, :3])
+
+    @raises(RuntimeError)
+    def test_transform_raises(self):
+        nlm = GeodesicNLM(n_coords = 2, mapping_kind = None, n_neighbors = 3)
+        nlm.fit(samples[:3])
+        nlm.transform(samples[0])
+
+    def test_transform(self):
+        nlm = GeodesicNLM(n_coords = 2, n_neighbors = 3)
+        nlm.fit(samples[:3])
+        mapped = nlm.transform(samples)
+        assert_array_almost_equal(mapped[:3], nlm.embedding_, decimal=3)
 
 if __name__ == "__main__":
   unittest.main()
